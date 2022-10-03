d95007f194c1f61f2c2f83f387d1edcfdb99d140
setiaonce <gedeyoga1126@gmail.com>
2022-10-03 19:12:02 +0700

    menambahkan paragraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba get remote</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus voluptas reiciendis quod est id, minima ex ea earum dolorem aliquam deleniti laboriosam. Similique excepturi cum quaerat repellendus commodi modi repellat?</p>
+</body>
+</html>
